151b8ae9241ddd70dc779c69ec81ce583134e642
Bas Heerschop <heerschop@gmail.com>
2023-06-15 12:50:38 +0200

    firebase

--- src/components/timetable/timetable.tsx
+++ src/components/timetable/timetable.tsx
@@ -1,11 +1,15 @@
-import { component$ } from '@builder.io/qwik';
-import styles from './styles.module.css';
+import { component$ } from "@builder.io/qwik";
+import styles from "./styles.module.css";
 
 export const Timetable = component$(() => {
   return (
     <>
-      <div class={styles['container']}>
-        <h1>Timetable</h1>
+      <div class={styles["container"]}>
+        <div class={styles["title"]}>Rooster</div>
+        <div class={styles["rooster"]}>
+          <div>Log in om je rooster te bekijken.</div>
+          <button>Inloggen</button>
+        </div>
       </div>
     </>
   );
